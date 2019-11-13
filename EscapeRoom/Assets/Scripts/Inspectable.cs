efd23012099e3ed0862f26f324cac7f7821316d6
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2019-11-13 14:41:56 +0000

    Inspection and safe asset
    Completed tasks:
    - Player can now inspect items by pressing E when near an interable object
    - New safe asset with electronic keypad

---
+++ EscapeRoom/Assets/Scripts/Inspectable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Inspectable : MonoBehaviour
+{
+    Camera inspectionCamera;
+
+    private void Awake()
+    {
+        inspectionCamera = GetComponentInChildren<Camera>();
+    }
+
+    private void Start()
+    {
+        ActivateCamera(false);
+    }
+
+    public void ActivateCamera(bool isActive)
+    {
+        inspectionCamera.gameObject.SetActive(isActive);
+    }
+
+    public bool GetIsCameraActive()
+    {
+        return inspectionCamera.isActiveAndEnabled;
+    }
+}
---
+++ EscapeRoom/Assets/Scripts/Inspector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inspector : MonoBehaviour
+{
+    Camera playerCamera;
+    LayerMask interactablesLayer;
+    Mover mover;
+
+    [SerializeField] float maxInspectionDistance = 4f;
+
+    private void Awake()
+    {
+        playerCamera = GetComponentInChildren<Camera>();
+        mover = GetComponent<Mover>();
+    }
+
+    private void Start()
+    {
+        interactablesLayer = LayerMask.GetMask("Interactable");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+
+            if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out RaycastHit hit, maxInspectionDistance, interactablesLayer))
+            {
+                Inspectable inspectable = hit.collider.gameObject.GetComponent<Inspectable>();
+
+                if (inspectable == null) return;
+
+                bool isInspecting = inspectable.GetIsCameraActive();
+
+                if (isInspecting)
+                {
+                    mover.IsFrozen = false;
+                    inspectable.ActivateCamera(false);
+                }
+                else
+                {
+                    mover.IsFrozen = true;
+                    inspectable.ActivateCamera(true);
+                }
+            }
+        }
+    }
+}
